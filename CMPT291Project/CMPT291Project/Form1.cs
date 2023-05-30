856f5da2503399f1db30fb986bc038292ee10b95
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2023-05-30 10:56:34 -0600

    Add files via upload

--- CMPT291Project/CMPT291Project/Form1.cs
+++ CMPT291Project/CMPT291Project/Form1.cs
@@ -1,8 +1,9 @@
 namespace CMPT291Project
 {
-    public partial class Form1 : Form
+    public partial class form_login : Form
     {
-        public Form1()
+
+        public form_login()
         {
             InitializeComponent();
         }
@@ -24,8 +25,30 @@ namespace CMPT291Project
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form2 F2 = new Form2();
-            F2.Show();
+            if (cb_type.Text == "Customer" && text_user.Text == "user" && text_pw.Text == "password")
+            {
+                Form2 F2 = new Form2();
+                F2.Show();
+                F2.
+            }
+            else if (cb_type.Text == "Customer" && text_user.Text == "admin" && text_pw.Text == "admin")
+            {
+            }
+            {
+                MessageBox.Show("Invalid username or password", "please try again", MessageBoxButtons.OK);
+                label_invalid.Text = "Invalid Username or Password, please try again.";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- CMPT291Project/CMPT291Project/Form2.cs
+++ CMPT291Project/CMPT291Project/Form2.cs
@@ -21,15 +21,5 @@ namespace CMPT291Project
         {
 
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
     }
 }
