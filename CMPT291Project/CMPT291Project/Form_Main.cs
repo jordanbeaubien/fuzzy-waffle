74f259e0ed0df9417fb6d6bf3a134e67a49c716c
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2023-06-01 12:05:04 -0600

    After todays lab

--- CMPT291Project/CMPT291Project/Form_Main.cs
+++ CMPT291Project/CMPT291Project/Form_Main.cs
@@ -7,15 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace CMPT291Project
 {
     public partial class Form2 : Form
     {
+        public SqlConnection sqlConnection;
+        public SqlCommand sqlCommand;
+        public SqlDataReader sqlReader;
+
+        /*
+        public bool IsUserAuthenticated { get; set; } // Added login auth
+        */
+
         public Form2()
         {
             InitializeComponent();
+            String connectionString = "Server = HAMMERPAD; Database = CMPT291Project; Trusted_Connection = yes";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            try
+            {
+                sqlConnection.Open(); // Open connection
+                sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection; // Link the command stream to the connection
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+                this.Close();
+            }
+
+            /*
+            // Wire up the SelectedIndexChanged eevent of the TabControl
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            */
         }
+
+
+        /*
+        // Login private code
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!IsUserAuthenticated && (tabControl1.SelectedTab == tab_car || tabControl1.SelectedTab == tab_query))
+            {
+                MessageBox.Show("Unable to load tab. You have insufficient access privileges.");
+                tabControl1.SelectedTab = tab_rental;
+            }
+        }
+        */
 
         private void tabPage1_Click(object sender, EventArgs e)
         {
@@ -38,6 +82,65 @@ namespace CMPT291Project
         }
 
         private void search_button_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pickup_date_picker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //   if cb_type
+        }
+
+        private void button_confirm_Click(object sender, EventArgs e)
+        {
+            if (button_add.Checked)
+            {
+                sqlCommand.CommandText = "insert into Car values ('"+ vin.Text + "','" + 
+                    make.Text + "','" + model.Text + "'," + year.Text + ",'" + 
+                    colour.Text + "','" + license.Text + "'," + branch.Text + ",'" + type.Text + "')";
+
+                MessageBox.Show(sqlCommand.CommandText);
+                
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        private void button_add_CheckedChanged(object sender, EventArgs e)
         {
 
         }
